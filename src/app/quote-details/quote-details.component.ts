e4b9b456d4966dcf253725307c1c697609e79998
FISTARGILLY2643 <fitstargilii1194@gmail.com>
2021-05-14 17:17:53 +0300

    Bind the quotes component with quote-details component

---
+++ src/app/quote-details/quote-details.component.ts
@@ -0,0 +1,20 @@
+// import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { Quote } from '../quote'; 
+
+@Component({
+  selector: 'app-quote-details',
+  templateUrl: './quote-details.component.html',
+  styleUrls: ['./quote-details.component.css']
+})
+export class QuoteDetailsComponent implements OnInit {
+
+  @Input() quoty: Quote;
+  @Output() isRead = new EventEmitter<boolean>();
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
