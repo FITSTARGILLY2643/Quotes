0c7a599eb256b40950ce83cf0b9196ef57317562
FISTARGILLY2643 <fitstargilii1194@gmail.com>
2021-05-14 17:02:03 +0300

    Create an array of quotes

---
+++ src/app/quotes/quotes.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+import { Quote } from '../quote';
+
+@Component({
+  selector: 'app-quotes',
+  templateUrl: './quotes.component.html',
+  styleUrls: ['./quotes.component.css']
+})
+export class QuotesComponent implements OnInit {
+
+  title = 'Awesome quotes';
+  quotes:Quote[] = [
+    new Quote (1,'Charles','Human spirit','The human spirit must prevail over technology to allow humans be in charge.','Albert Einstein', new Date(2019,7,12),0,0),
+    
+  ];
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
