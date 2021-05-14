894b7c24f3e76b2d778515a2f21dd6de92c94e64
FISTARGILLY2643 <fitstargilii1194@gmail.com>
2021-05-14 17:05:00 +0300

    Create a function that delete a quote

--- src/app/quotes/quotes.component.ts
+++ src/app/quotes/quotes.component.ts
@@ -26,6 +26,16 @@ export class QuotesComponent implements OnInit {
     quote.completeDate = new Date(quote.completeDate)
     this.quotes.push(quote)
   }
+
+  quoteDelete(isRead, index){
+    if (isRead) {
+      let toDelete = confirm(`Are you sure you want to delete this Quote?`)
+      if(toDelete){
+        this.quotes.splice(index,1);
+      }
+      
+    }
+  }
   constructor() { }
 
   ngOnInit(): void {
